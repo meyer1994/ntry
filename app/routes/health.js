2c643aee8985e61d0ece2b11025be40b391450b3
Joao Meyer <1994meyer@gmail.com>
2020-07-14 01:33:00 -0300

    fixed IPFS healtcheck

--- app/routes/health.js
+++ app/routes/health.js
@@ -2,7 +2,7 @@ const { w3 } = require('../services/eth')
 const { ipfs } = require('../services/ipfs')
 
 module.exports.ipfs = async ctx => {
-  ctx.body = await ipfs.getEndpointConfig()
+  ctx.body = await ipfs.repo.stat()
 }
 
 module.exports.eth = async ctx => {
