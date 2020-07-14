237e0408400d67c25d4dbc07637236091c569a79
Joao Meyer <1994meyer@gmail.com>
2020-07-14 00:46:03 -0300

    Created a endpoint to register birth

--- t.js
+++ t.js
@@ -1,21 +1,23 @@
 const Koa = require('koa')
 const Router = require('@koa/router')
+const bodyParser = require('koa-bodyparser');
 
 const Web3 = require('web3')
 const IPFS = require('ipfs-http-client')
 
 const TruffleContract = require('@truffle/contract')
-const SumABI = require('./build/contracts/Sum.json')
+const BirthABI = require('./build/contracts/Birth.json')
 
 const w3 = new Web3('http://localhost:8545')
 const ipfs = new IPFS('http://localhost:5001')
 
-const Sum = TruffleContract(SumABI)
-Sum.setProvider(w3.currentProvider)
+const Birth = TruffleContract(BirthABI)
+Birth.setProvider(w3.currentProvider)
 
 const app = new Koa()
 const router = new Router()
 
+app.use(bodyParser())
 
 router.get('/ipfs', async ctx => {
   ctx.body = await ipfs.getEndpointConfig()
@@ -25,12 +27,26 @@ router.get('/eth', async ctx => {
   ctx.body = await w3.eth.getNodeInfo()
 })
 
-router.get('/sum', async ctx => {
-  const instance = await Sum.deployed()
-  ctx.body = await instance.sum(1, 2)
+router.post('/register/birth', async ctx => {
+  const { name, date } = ctx.request.body
+
+  const options = { from: w3.eth.defaultAccount }
+  const birth = await Birth.new(date, name, options)
+
+  const body = {
+    address: birth.address,
+    name: await birth.name(),
+    date: await birth.date(),
+    kind: await birth.kind(),
+  }
+
+  ctx.body = body
 })
 
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-app.listen(3000)
+app.listen(3000, async () => {
+  const [ account ] = await w3.eth.getAccounts()
+  w3.eth.defaultAccount = account
+})
