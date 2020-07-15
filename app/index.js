ba57c5cf7569407f7c286a2ea169b69d6927a740
Joao Meyer <1994meyer@gmail.com>
2020-07-15 00:22:42 -0300

    Added logging middleware

--- app/index.js
+++ app/index.js
@@ -1,15 +1,20 @@
 const Koa = require('koa')
-const bodyParser = require('koa-bodyparser');
+const bodyParser = require('koa-bodyparser')
+const accesslog = require('koa-accesslog')
 
 const { router } = require('./routes')
 
 const app =  new Koa()
 
+// Middlewares
 app.use(bodyParser())
+app.use(accesslog())
 
+// Routes
 app.use(router.routes())
 app.use(router.allowedMethods())
 
+// Start
 app.listen(3000, () => {
   console.log('App started')
 })
