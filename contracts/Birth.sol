bfee01399a5199bee90e087637bcde5f0c98e2b7
Joao Meyer <1994meyer@gmail.com>
2020-07-14 21:59:44 -0300

    Changed version

--- contracts/Birth.sol
+++ contracts/Birth.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity ^0.7.0;
 
 import { Record } from "./Record.sol";
 
@@ -6,7 +6,7 @@ contract Birth is Record {
     uint public date;
     string public name;
 
-    constructor (uint _date, string memory _name) public {
+    constructor(uint _date, string memory _name) public {
         date = _date;
         name = _name;
     }
