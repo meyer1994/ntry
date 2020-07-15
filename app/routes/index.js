e2aad0c6dff29366cdfa39eabda278c73e5b7bb2
Joao Meyer <1994meyer@gmail.com>
2020-07-14 23:13:01 -0300

    Added other endpoints

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,18 +2,26 @@ const Router = require('@koa/router')
 
 const health = require('./health')
 
-const birth = require('./contracts/birth')
-const death = require('./contracts/death')
+const birth = require('./contracts.birth')
+const death = require('./contracts.death')
+const divorce = require('./contracts.divorce')
+const marriage = require('./contracts.marriage')
 
 const router = new Router()
 
-router.get('/health/ipfs', health.getIpfs)
 router.get('/health/eth', health.getEth)
+router.get('/health/ipfs', health.getIpfs)
 
 router.post('/docs/births', birth.postBirth)
 router.get('/docs/births/:addr', birth.getBirth)
 
 router.post('/docs/deaths', death.postDeath)
 router.get('/docs/deaths/:addr', death.getDeath)
+
+router.post('/docs/divorces', divorces.postDivorces)
+router.get('/docs/divorces/:addr', divorces.getDivorces)
+
+router.post('/docs/marriages', marriages.postMarriages)
+router.get('/docs/marriages/:addr', marriages.getMarriages)
 
 module.exports.router = router
