cb94659ccdf6f2c6fc213ae337a113dc5fbf433c
Joao Meyer <1994meyer@gmail.com>
2020-07-14 21:59:54 -0300

    Added death contract

---
+++ contracts/Death.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.7.0;
+
+import { Record } from "./Record.sol";
+
+contract Death is Record {
+    uint public time;
+    Birth public birth;
+
+    constructor(uint _time, Birth _birth) public {
+        time = _time;
+        birth = _birth;
+    }
+
+    function kind() public override view returns(string memory) {
+        return "Death";
+    }
+}
