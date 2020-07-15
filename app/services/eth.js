399a9afd41475a345869899a62235cc2eb3485e1
Joao Meyer <1994meyer@gmail.com>
2020-07-14 22:42:55 -0300

    Set default account on boot

--- app/services/eth.js
+++ app/services/eth.js
@@ -7,5 +7,10 @@ const BirthABI = require('../../build/contracts/Birth.json')
 const Birth = Contract(BirthABI)
 Birth.setProvider(w3.currentProvider)
 
+w3.eth.getAccounts().then(([ acc ]) => {
+  console.log('Setting default account to:', acc)
+  w3.eth.defaultAccount = acc
+})
+
 module.exports.w3 = w3
 module.exports.Birth = Birth
