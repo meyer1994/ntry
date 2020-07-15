bf6491a72f66d14a110913335a148dde9ecd63df
Joao Meyer <1994meyer@gmail.com>
2020-07-14 22:55:41 -0300

    Added death contract

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,7 @@ const Router = require('@koa/router')
 const health = require('./health')
 
 const birth = require('./contracts/birth')
+const death = require('./contracts/death')
 
 const router = new Router()
 
@@ -11,5 +12,8 @@ router.get('/health/eth', health.getEth)
 
 router.post('/docs/births', birth.postBirth)
 router.get('/docs/births/:addr', birth.getBirth)
+
+router.post('/docs/deaths', death.postDeath)
+router.get('/docs/deaths/:addr', death.getDeath)
 
 module.exports.router = router
--- app/services/eth.js
+++ app/services/eth.js
@@ -2,15 +2,20 @@ const Web3 = require('web3')
 const Contract = require('@truffle/contract')
 
 const w3 = new Web3('http://localhost:8545')
+module.exports.w3 = w3
 
 const BirthABI = require('../../build/contracts/Birth.json')
 const Birth = Contract(BirthABI)
 Birth.setProvider(w3.currentProvider)
+module.exports.Birth = Birth
+
+const DeathABI = require('../../build/contracts/Death.json')
+const Death = Contract(DeathABI)
+Death.setProvider(w3.currentProvider)
+module.exports.Death = Death
 
 w3.eth.getAccounts().then(([ acc ]) => {
   console.log('Setting default account to:', acc)
   w3.eth.defaultAccount = acc
 })
 
-module.exports.w3 = w3
-module.exports.Birth = Birth
