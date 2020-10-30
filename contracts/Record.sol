84db484c31c1f7b8dafe0b15351a542bbac0b882
Joao Meyer <1994meyer@gmail.com>
2020-10-29 22:44:49 -0300

    Removed truffle things

--- contracts/Record.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity >0.6.4 <0.7.0;
-
-abstract contract Record {
-    string ipfs;
-
-    function setIpfs(string memory _ipfs) public {
-        ipfs = _ipfs;
-    }
-}
--- migrations/1_initial_migration.js
+++
@@ -1,5 +0,0 @@
-const Migrations = artifacts.require('Migrations')
-
-module.exports = async deployer => {
-  deployer.deploy(Migrations)
-}
